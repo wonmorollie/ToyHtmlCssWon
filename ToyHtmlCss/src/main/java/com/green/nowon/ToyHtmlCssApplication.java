5ffc053012b784e3bc47daae5922f14941120288
Wonmo <dnjsah1313@gmail.com>
2022-12-07 19:40:07 +0900

    1207

---
+++ ToyHtmlCss/src/main/java/com/green/nowon/ToyHtmlCssApplication.java
@@ -0,0 +1,13 @@
+package com.green.nowon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyHtmlCssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyHtmlCssApplication.class, args);
+	}
+
+}
